954c6ee29854d432fa6e173972c7a8b73521b34d
SShan2021 <86134251+SShan2021@users.noreply.github.com>
2022-12-05 20:42:10 -0500

    This is updated code

--- code/tidy_nashp.R
+++ code/tidy_nashp.R
@@ -2,41 +2,106 @@
 #setwd()
 setwd("~/work")
 
+################################################
 #load libraries
 library(readxl)
 library(tidyverse)
 library(vistime)
-webshot::install_phantomjs()
+library(lubridate)
+library(plotly)
+webshot::install_phantomjs(force = TRUE)
 
-#set working directory
-#"gear" tab in the bottom right pane of rstudio
-#(more -> set current working directory)
-#READ.me should say that this is the working directory
-
+################################################
 #read in RAND data
 nashp_raw <- read_xlsx("source_data/NASHP_HCT_Data_2022_April.xlsx",
-                      sheet = "HCT data")
-head(nashp_raw)
-dim(nashp_raw) #42709 rows, 65 columns
+                       sheet = "HCT data")
+dim(nashp_raw)
 
+################################################
 #make colnames lowercase
 colnames(nashp_raw) <- str_to_lower(colnames(nashp_raw)) %>% str_trim();
 #replace all whitespace with underscores
 colnames(nashp_raw) <- str_replace_all(colnames(nashp_raw), "[[:space:],#'$?/()-=]+", "_") %>%
   str_replace_all("[_]+$","");
 
-#check out the most recent year
-nashp_raw_2019 <- nashp_raw %>%
-  filter(year == "2019")
+################################################
+#how many unique hospitals
+length(unique(nashp_raw$ccn)) 
 
-nashp_raw_2019_fiscalyear <- nashp_raw %>%
-  filter(year == "2019") %>%
-  group_by(ccn) %>% #among one ccn, I would like
-  #to get the smallest fiscal year beginning
-  summarise(fiscal_year_beginning = min(fiscal_year_beginning),
-            fiscal_year_ending = max(fiscal_year_ending)) %>% head(20)
+################################################
+#subsetting to only 2016-2018 first
+nashp_raw_2016_2018 <- nashp_raw %>%
+  filter(year == 2016 |
+           year == 2017 |
+           year == 2018)
 
+length(unique(nashp_raw_2016_2018$ccn)) 
+################################################
+#figuring out fiscal year distribution
+#selecting columns of interest
+nashp_2016_2018_fiscalyear <- nashp_raw_2016_2018 %>%
+  select(year, ccn, fiscal_year_beginning, fiscal_year_ending)
 
-f <- vistime(nashp_raw_2019_fiscalyear, col.event = "ccn",
-        col.start = "fiscal_year_beginning", col.end = "fiscal_year_ending")
-k <- plotly::export(f, file="figures/fiscal_year_2019.png")
+#how many days between start and end of fiscal year?
+nashp_2016_2018_fiscalyear <- nashp_2016_2018_fiscalyear %>%
+  mutate(difference = as.numeric(difftime(ymd(fiscal_year_ending),
+                                          ymd(fiscal_year_beginning),
+                                          units = "days"))) 
+
+################################################
+#take a look at distribution of fiscal year length 
+nashp_2016_2018_fiscalyear %>%
+  group_by(difference) %>%
+  tally() %>%
+  arrange(desc(difference)) 
+#there are fiscal years that are much greater than 364 or 365
+#there are also fiscal years that mare much smaller than 364 or 365
+#notice that the majority of the fiscal years are either 364 or 365
+
+#need to extract all rows with fiscal years < 364 or >365 
+nashp_2016_2018_fiscalyear_notnormal <- nashp_2016_2018_fiscalyear %>%
+  filter(difference < 364|
+           difference > 365) %>%
+  select(ccn) %>%
+  distinct()
+
+dim(nashp_2016_2018_fiscalyear_notnormal)
+
+#select out those hospitals and take a look at them
+nashp_2016_2018_fiscalyear_notnormal_data <- nashp_raw_2016_2018 %>% 
+  filter(ccn %in% nashp_2016_2018_fiscalyear_notnormal$ccn) %>%
+  arrange(ccn)
+
+length(unique(nashp_2016_2018_fiscalyear_notnormal_data$ccn)) 
+dim(nashp_2016_2018_fiscalyear_notnormal_data) 
+
+################################################
+#take a look at distribution of days (for strange
+#hospitals)
+nashp_2016_2018_fiscalyear_notnormal_data_top30 <- nashp_raw_2016_2018 %>%
+  filter(ccn %in% nashp_2016_2018_fiscalyear_notnormal_data$ccn) %>%
+  select(year, ccn, fiscal_year_beginning, fiscal_year_ending) %>%
+  arrange(ccn) %>%
+  mutate(difference = as.numeric(difftime(ymd(fiscal_year_ending),
+                                          ymd(fiscal_year_beginning),
+                                          units = "days"))) %>%
+  group_by(ccn) 
+
+f_2 <- vistime(nashp_2016_2018_fiscalyear_notnormal_data_top30[1:20,], col.group = "ccn",
+               col.event = "difference", 
+               col.start = "fiscal_year_beginning", col.end = "fiscal_year_ending")
+plotly::export(f_2, file = "figures/nashp_strangefiscalyear.pdf")
+
+################################################
+#extract normal hospitals
+nashp_2016_2018_fiscalyear_normal_data <- nashp_raw_2016_2018 %>% 
+  filter(!(ccn %in% nashp_2016_2018_fiscalyear_notnormal$ccn)) %>%
+  arrange(ccn) %>%
+  #exclude hospitals in the District of Columbia
+  filter(state != "DC")
+
+length(unique(nashp_2016_2018_fiscalyear_normal_data$ccn)) 
+
+################################################
+write_csv(nashp_2016_2018_fiscalyear_normal_data,
+                      "./derived_data/tidy_nashp_2016_2018.csv")
--- code/tidy_rand.R
+++ code/tidy_rand.R
@@ -2,32 +2,31 @@
 #setwd
 setwd("~/work")
 
-
+################################################
 #load libraries
 library(readxl)
 library(tidyverse)
 
-#set working directory
-#"gear" tab in the bottom right pane of rstudio
-#(more -> set current working directory)
-#READ.me should say that this is the working directory
-
+################################################
 #read in RAND data
 rand_raw <- read_xlsx("source_data/Supplemental_Material.xlsx",
                       sheet = "Table 1. PF Hospitals",
                       skip = 9)
 head(rand_raw)
-dim(rand_raw) #3155 rows, 29 columns
+dim(rand_raw) 
 
+################################################
 #make colnames lowercase
 colnames(rand_raw) <- str_to_lower(colnames(rand_raw)) %>% str_trim();
 #replace all whitespace with underscores
 colnames(rand_raw) <- str_replace_all(colnames(rand_raw), "[[:space:],$?/()-=]+", "_") %>%
   str_replace_all("[_]+$","");
 
+################################################
 #are the medicare provider numbers unique? yes
 rand_raw %>% group_by(medicare_provider_number) %>% tally() %>% filter(n!=1)
 
+################################################
 #are the hospital names unique?
 #in some states the hospital names are the same
 rand_raw %>%
@@ -35,18 +34,6 @@ rand_raw %>%
   tally() %>%
   filter(n!=1)
 
-#meta-data about hospital
-rand_metadata <- rand_raw %>%
-  select(medicare_provider_number, hospital_name, street_address, city, state, zip_code,
-         hospital_system_or_if_independent_ipps_cah, is_hospital_a_critical_access_hospital_y_n,
-         hospital_compare_star_rating_october_na_not_available)
-
-#deleting the meta-data from the RAND file
-rand_raw <- rand_raw %>% select(-hospital_name, -street_address, -city, -state, -zip_code,
-                                -hospital_system_or_if_independent_ipps_cah,
-                                -is_hospital_a_critical_access_hospital_y_n,
-                                -hospital_compare_star_rating_october_na_not_available)
-
-#save files to edited_data
-write_csv(rand_raw, "./derived_data/rand_raw.csv")
-write_csv(rand_metadata, "./derived_data/rand_metadata.csv")
+################################################
+#save files to derived_data
+write_csv(rand_raw, "./derived_data/tidy_rand.csv")
